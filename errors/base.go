6041ae7b82c09bd094152460ebf8653f6869752e
benyaa <ben2460@gmail.com>
2024-08-28 23:12:10 +0300

    removed redundant errors

--- errors/base.go
+++ errors/base.go
@@ -3,8 +3,6 @@ package errors
 import "fmt"
 
 var CouldNotDeepCopyFlowObject = fmt.Errorf("could not deep copy flow object")
-var FailedToGenerateNewFileHandler = fmt.Errorf("failed to generate new file handler")
-var ProcessorNotFound = fmt.Errorf("failed to find processor")
 var ErrProcessorFailed = fmt.Errorf("processor failed")
 var FailedToGetNextProcessor = fmt.Errorf("failed to get next processor")
 var CouldNotCreateFlowManager = fmt.Errorf("could not create flow manager")
