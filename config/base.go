139220b1bd565b7f67f374ac44fa0725604d345b
benyaa <ben2460@gmail.com>
2024-08-30 14:38:36 +0300

    removed redundant structs

--- config/base.go
+++ config/base.go
@@ -1,23 +1,6 @@
 package config
 
-import (
-	"github.com/go-streamline/interfaces/definitions"
-	"time"
-)
-
 type Config struct {
-	Workdir          string            `json:"workdir"`
-	Processors       []ProcessorConfig `json:"processors"`
-	MaxWorkers       int               `json:"maxWorkers"`
-	InitRetryBackOff time.Duration     `json:"initRetryBackOff"`
-}
-
-type ProcessorConfig struct {
-	Processor definitions.Processor
-	Retry     ProcessorRetryMechanism
-}
-
-type ProcessorRetryMechanism struct {
-	MaxRetries      int
-	BackOffInterval time.Duration
+	Workdir    string `json:"workdir"`
+	MaxWorkers int    `json:"maxWorkers"`
 }
--- config/db.go
+++
@@ -1,6 +0,0 @@
-package config
-
-type DBConfig struct {
-	Driver string
-	DSN    string
-}
